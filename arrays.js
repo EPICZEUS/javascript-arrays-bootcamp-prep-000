27526f5bcc20db0d006202ff5bdd9571840465cf
Sam Brozinsky <s77broz@aol.com>
2018-08-28 06:51:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,12 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
 
 function addElementToBeginningOfArray(array, ele) {
-  return array.unshift(ele);
+  return [ele, ...array];
 }
 
 function destructiveAddElementToBeginningOfArray(array, ele) {
-  return [ele, ...array];
+  array.unshift(ele);
+  return array;
 }
 
 function addElementToEndOfArray(array, ele) {
